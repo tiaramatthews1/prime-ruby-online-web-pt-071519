49c90299018aee3789df7f8f80932e6143a2b686
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-24 02:28:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  prime.each do |num|
+    num.prime?(2)
+end
+
+def prime?(num)
+ = prime
+[2,3,11,105557].each do |number|
+  if num.prime? 
+    all_num = true
+  end
+end
