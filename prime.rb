c9a5d086c65f17f63c4550956355235bc70dbd59
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-24 02:48:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,13 @@
 require 'prime'
 
-
-
-
-
-
-
-
+def is_prime(num)
+  n = 2
+  while n < num
+    return false if num % n == 0
+    n += 1
+  end
+  true
+end
 
 def is_prime(num)
   (2..(num - 1)).each do |n|
