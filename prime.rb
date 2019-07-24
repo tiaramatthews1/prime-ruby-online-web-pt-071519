0472b56e1f49b4cc9ab7ba93a8278f1f4662cb4f
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-24 02:39:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,5 @@
 require 'prime'
 
 def prime?(num)
-return false if n < 2 
-  return true if n == 3 || n == 2 
-    if (2...n-1).any?{|i| n % i == 0}
-      false
-    else
-      true
-    end
+
 end
